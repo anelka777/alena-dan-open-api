eabdad497b7b0d665dbe21020ecac30c1f26aecd
anelka777 <46974606+anelka777@users.noreply.github.com>
2024-09-18 23:35:53 -0700

    added a third button with functionality in the js file and some styles in the CSS file

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+
+
 function interpretWeatherCode(code) {
     const weatherConditions = {
         0: 'Clear sky',
@@ -53,14 +55,14 @@ function getWeatherImagePath(code) {
         71: 'slight_snow.png',
         73: 'moderate_snow.png',
         75: 'heavy_snow.png',
-        80: 'rain_showers_slight.png',
-        81: 'rain_showers_moderate.png',
-        82: 'rain_showers_violent.png',
+        80: 'rain_showers_slight.png', // -
+        81: 'rain_showers_moderate.png', // -
+        82: 'rain_showers_violent.png', // -
         85: 'slight_snow_showers.png',
         86: 'heavy_snow_showers.png',
-        95: 'thunderstorm.png',
-        96: 'thunderstorm_with_slight_hail.png',
-        99: 'thunderstorm_with_heavy_hail.png',
+        95: 'thunderstorm.png', // -
+        96: 'thunderstorm_with_slight_hail.png', // -
+        99: 'thunderstorm_with_heavy_hail.png', // -
     };
 
     return weatherImages[code] || 'default.png';
@@ -92,46 +94,77 @@ function updateTomorrowWeatherDisplay(time, temperature, weatherCode) {
     document.getElementById('weather_image_tommorow').src = `./images/${weatherImagePath}`;
 }
 
+
 async function fetchData() {
     try {
-        const response = await fetch('https://api.open-meteo.com/v1/forecast?latitude=38.5816&longitude=-121.4944&current_weather=true&daily=weather_code,temperature_2m_max,temperature_2m_min,rain_sum&temperature_unit=fahrenheit&timezone=America%2FLos_Angeles');
+        const response = await fetch('https://api.open-meteo.com/v1/forecast?latitude=38.5816&longitude=-121.4944&current=temperature_2m,weather_code&hourly=temperature_2m,weather_code&daily=weather_code,temperature_2m_max,temperature_2m_min&temperature_unit=fahrenheit&timezone=America%2FLos_Angeles');
         if (!response.ok) {
             throw new Error('Request failed');
         }
 
         const data = await response.json();
-        const timeNow = new Date(data.current_weather.time);
-        const temperatureNow = data.current_weather.temperature;
-        const weatherCodeNow = data.current_weather.weathercode;
+        console.log(data);
+
+        const timeNow = new Date(data.current.time);
+        const temperatureNow = data.current.temperature_2m;
+        const weatherCodeNow = data.current.weather_code;
+
         const timeTomorrow = new Date(data.daily.time[2]);
         const temperatureTomorrow = data.daily.temperature_2m_max[2];
         const weatherCodeTomorrow = data.daily.weather_code[2];
 
+        const hourlyTime = data.hourly.time;
+        const hourlyTemperature = data.hourly.temperature_2m;
+        const hourlyWeatherCode = data.hourly.weather_code;
+/*
+        console.log('Текущее локальное время:', new Date());
+        console.log('Дата, полученная из API (сегодня):', new Date(data.daily.time[0]));
+        console.log('Дата, полученная из API (завтра):', new Date(data.daily.time[1]));
+        console.log('Дата из API:', new Date(data.daily.time[1]));
+        console.log('Текущее время (UTC):', new Date().toISOString());
+        console.log('Текущее локальное время:', new Date());
+*/
+
         updateWeatherDisplay(timeNow, temperatureNow, weatherCodeNow);
         updateTomorrowWeatherDisplay(timeTomorrow, temperatureTomorrow, weatherCodeTomorrow);
+        displayHourlyWeather(hourlyTime, hourlyTemperature, hourlyWeatherCode);
 
         document.getElementById('toggle-button').addEventListener('click', function() {
             const currentWeatherDiv = document.getElementById('current-weather');
             const tomorrowWeatherDiv = document.getElementById('tomorrow-weather');
             const header = document.getElementById('weather-header');
-
             if (currentWeatherDiv.classList.contains('hidden')) {
                 currentWeatherDiv.classList.remove('hidden');
                 tomorrowWeatherDiv.classList.add('hidden');
                 this.textContent = 'Show Tomorrow\'s Weather';
-                header.textContent = 'Weather Now:';
+                header.textContent = 'Now:';
             } else {
                 currentWeatherDiv.classList.add('hidden');
                 tomorrowWeatherDiv.classList.remove('hidden');
                 this.textContent = 'Show Current Weather';
-                header.textContent = 'Weather Tomorrow:';
+                header.textContent = 'Tomorrow:';
             }
         });
 
+        document.getElementById('toggle-button-daily').addEventListener('click', function() {
+            const dailyWeatherDiv = document.getElementById('forecast');
+            const dailyHeader = document.getElementById('weather-header-daily');
+            if (dailyWeatherDiv.classList.contains('hidden')) {
+                dailyWeatherDiv.classList.remove('hidden');
+                dailyHeader.classList.remove('hidden');
+                this.textContent = 'Hide Weather Daily'
+                dailyHeader.textContent = 'Daily:';
+            } else {
+                dailyWeatherDiv.classList.add('hidden');
+                dailyHeader.classList.add('hidden');
+                this.textContent = 'Show Weather Daily';
+            }
+        })
+
         const forecastContainer = document.getElementById('forecast');
         forecastContainer.innerHTML = '';
-
-        const currentDay = new Date(data.daily.time[1]).getDate();
+        const currentDay = new Date(data.daily.time[0]).getDate();
+        
         for (let i = 1; i < data.daily.time.length && i <= 7; i++) {
             const week = new Date(data.daily.time[i]);
             if (week.getDate() !== currentDay) {
@@ -154,9 +187,59 @@ async function fetchData() {
             }
         }
 
+        document.getElementById('toggle-button-hourly').addEventListener('click', function() {
+            const hourlyWeatherDiv = document.getElementById('today-hourly');
+            const hourlyHeader = document.getElementById('weather-header-hourly');
+            if (hourlyWeatherDiv.classList.contains('hidden')) {
+                hourlyWeatherDiv.classList.remove('hidden');
+                hourlyHeader.classList.remove('hidden');
+                this.textContent = 'Hide Today\'s Weather Hourly'
+                hourlyHeader.textContent = 'Hourly:';
+            } else {
+                hourlyWeatherDiv.classList.add('hidden');
+                hourlyHeader.classList.add('hidden');
+                this.textContent = 'Show Today\'s Weather Hourly';
+            }
+        })
+    
+        function displayHourlyWeather(hourlyTime, hourlyTemperature, hourlyWeatherCode) {
+            const now = new Date();
+            const currentHour = now.getHours();
+            const currentHourIndex = hourlyTime.findIndex(time => new Date(time).getHours() === currentHour);        
+            const hourlyContainer = document.getElementById('today-hourly');
+            hourlyContainer.innerHTML = '';
+
+            hourlyTime.slice(currentHourIndex, currentHourIndex + 12).forEach((time, i) => {
+                const formattedTime = new Date(time).toLocaleTimeString([], { hour: 'numeric', hour12: true });
+                const temperature = hourlyTemperature[currentHourIndex + i];
+                const weatherCondition = interpretWeatherCode(hourlyWeatherCode[currentHourIndex + i]);
+                const weatherImagePath = getWeatherImagePath(hourlyWeatherCode[currentHourIndex + i]);
+        
+                hourlyContainer.innerHTML += `
+                    <div class="forecast-item">
+                        <strong>${formattedTime}</strong><br>
+                        <strong>Temperature:</strong> ${temperature} °F<br>
+                        <strong>Condition:</strong> ${weatherCondition}<br>
+                        <img src="./images/${weatherImagePath}" alt="Weather condition" width="50px"><br>
+                    </div>
+                `;
+            });
+        }
+
     } catch (error) {
         console.error('An error occurred:', error);
     }
 }
 
 fetchData();
+
+
+// ============= footer==================
+const today = new Date();
+const thisYear = today.getFullYear();
+const footer = document.createElement('footer');
+const body = document.querySelector('body');
+body.appendChild(footer);
+const copyright = document.createElement('p');
+copyright.innerHTML = `&copy; Alena Danilchenko ${thisYear}`;
+footer.appendChild(copyright);
